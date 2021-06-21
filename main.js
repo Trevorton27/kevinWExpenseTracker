49df243e56e1031842671019ad1aaaabfee50000
Kevin W <kevinwestnyc@gmail.com>
2021-06-21 14:40:48 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+//make sure all inputs are filled in order to send
+// clear inputs after submission
+// add inputs to table
+// delete expense row 
+// sum of all amounts
+// pie chart from categories
