52dba35f0afbf263b7bd3b2327d947bcceef3500
Kevin W <kevinwestnyc@gmail.com>
2021-06-23 17:44:04 -0400

    added delete button but needs functionality

--- main.js
+++ main.js
@@ -1,12 +1,9 @@
-//make sure all inputs are filled in order to send
-// clear inputs after submission
-// add inputs to table
 // delete expense row 
 // sum of all amounts
 // pie chart from categories
 document.getElementById("form").addEventListener('submit', addExpense);
 document.getElementById("form").addEventListener('submit', resetForm);
-
+// document.getElementById("delete-button").addEventListener('click', removeItem);
 
 function addExpense (e) {
     e.preventDefault();
@@ -15,9 +12,10 @@ function addExpense (e) {
     const amount = document.getElementById("amount").value;
     const description = document.getElementById("description").value;
     const category = document.getElementById("category").value;
-    const table = document.getElementById("table")
-    const deleteButton = createDeleteButton();
-    const row = table.insertRow(1);
+    const table = document.getElementById("table-body")
+    const deleteButton = "<button id='delete-button' onclick='removeItem()' >X</button>"
+    
+    const row = table.insertRow(0);
     const cell1 = row.insertCell(0);
     const cell2 = row.insertCell(1);
     const cell3 = row.insertCell(2);
@@ -27,33 +25,26 @@ function addExpense (e) {
     
     cell1.innerHTML = date;
     cell2.innerHTML = location;
-    cell3.innerHTML = amount;
-    cell4.innerHTML = description;
+    cell3.innerHTML = description;
+    cell4.innerHTML = '$' + amount;
     cell5.innerHTML = category;
     cell6.innerHTML = deleteButton;
 }
 
-// function createTableRow(){
-//     const table = document.getElementById("table")
-//     const row = table.insertRow(0);
-//     var cell1 = row.insertCell(0);
-//     var cell2 = row.insertCell(1);
-//     cell1.innerHTML = "NEW CELL1";
-//     cell2.innerHTML = "NEW CELL2";
-
-
+// function createDeleteButton() {
+//     const deleteButton = document.createElement('button');
+//     deleteButton.className = 'delete-button';
+//     deleteButton.appendChild(document.createTextNode('X'));
+//     return deleteButton;
 // }
 
-function createDeleteButton() {
-    const deleteButton = document.createElement('button');
-    deleteButton.className = 'delete-button';
-    deleteButton.appendChild(document.createTextNode('X'));
-    return deleteButton;
+function removeItem(){
+    if(e.target.classList.contains('delete-button')){
+    const taskToRemove = e.target.parentElement;
+    taskToRemove.remove();
+    }
 }
 
-// function clearFields() {
-//     document.getElementsByClassName("expense-input").value = "";
-// }
 function resetForm() {
     document.getElementById("form").reset();
 }
